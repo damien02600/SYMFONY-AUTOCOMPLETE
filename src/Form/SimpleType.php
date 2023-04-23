54353caa8ebdf9d4cdd5e110479cff758bdf8f71
damien berthe <d.berthe1997@laposte.net>
2023-04-23 14:52:26 +0200

    SYMFONY-AUTOCOMPLETE

---
+++ src/Form/SimpleType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Simple;
+use Symfony\Component\Form\AbstractType;
+use App\Form\SimpleChoixAutocompleteField;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SimpleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('nom')
+        ->add('choix', SimpleChoixAutocompleteField::class)
+    ;
+}
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Simple::class,
+        ]);
+    }
+}
